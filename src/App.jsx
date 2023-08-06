19542c764c5e616979b9154e5581b8b76733c4cd
BasemYahia402 <basmyhy496@gmail.com>
2023-08-06 10:02:05 +0300

    Add Component Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import Meme from './Components/Meme/Meme'
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header/>
     <Meme/>
+    <Footer/>
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+export default function Footer(){
+    return(
+        <footer>
+            <p>
+                &copy; 2023, All rights reserved by ❤ Basem Yahia
+            </p>
+        </footer>
+    )
+}
